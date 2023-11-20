16c6f804550c7eb1a9c723fd5f47cdaa26dad19f
mo-zza <bigsmalljoe@gmail.com>
2023-11-20 22:00:57 +0900

    feat: introduce member repository custom

--- src/main/java/com/example/hexagonalarchitectureexample/adapter/out/persistence/repository/MemberRepository.java
+++ src/main/java/com/example/hexagonalarchitectureexample/adapter/out/persistence/repository/MemberRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, UUID> {
+public interface MemberRepository extends JpaRepository<Member, UUID>, MemberRepositoryCustom {
 }
