690e362c96850d2e5eb30083f46ce704f87d86d2
mo-zza <bigsmalljoe@gmail.com>
2023-11-20 22:01:17 +0900

    feat: implement insert rent data method

--- src/main/java/com/example/hexagonalarchitectureexample/adapter/out/persistence/RentalPersistenceAdapter.java
+++ src/main/java/com/example/hexagonalarchitectureexample/adapter/out/persistence/RentalPersistenceAdapter.java
@@ -2,7 +2,9 @@ package com.example.hexagonalarchitectureexample.adapter.out.persistence;
 
 import com.example.hexagonalarchitectureexample.adapter.out.persistence.repository.RentalRepository;
 import com.example.hexagonalarchitectureexample.application.domain.model.CampSite;
+import com.example.hexagonalarchitectureexample.application.domain.model.Member;
 import com.example.hexagonalarchitectureexample.application.domain.model.Rental;
+import com.example.hexagonalarchitectureexample.application.port.out.InsertRentPort;
 import com.example.hexagonalarchitectureexample.application.port.out.LoadRentCampSitePort;
 import com.example.hexagonalarchitectureexample.common.PersistenceAdapter;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +16,7 @@ import java.util.stream.Collectors;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
-public class RentalPersistenceAdapter implements LoadRentCampSitePort {
+public class RentalPersistenceAdapter implements LoadRentCampSitePort, InsertRentPort {
     private final RentalRepository rentalRepository;
 
     @Override
@@ -23,10 +25,19 @@ public class RentalPersistenceAdapter implements LoadRentCampSitePort {
         if (rentals.isEmpty()) {
             return Set.of();
         }
-        
+
         List<CampSite> campSites = rentals.stream()
                 .map(Rental::getCampSite)
                 .collect(Collectors.toList());
         return Set.copyOf(campSites);
+    }
+
+    @Override
+    public void insertRent(Member member, CampSite campSite) {
+        Rental rental = Rental.builder()
+                .member(member)
+                .campSite(campSite)
+                .build();
+        rentalRepository.save(rental);
     }
 }
